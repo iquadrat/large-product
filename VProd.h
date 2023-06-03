07fa0e21d4e071ce52ccb890b4eaeb45c5ad53c9
Micha Riser <micha@classtime.com>
2023-06-03 19:45:29 +0200

    improve ifs

--- VProd.h
+++ VProd.h
@@ -53,15 +53,15 @@ inline void checkoverflow(__m256d &prod, int64_t &exponent) {
   
   __m256d high_mask = _mm256_cmp_pd(abs_prod, toohigh, _CMP_GE_OS);
   __m256d low_mask  = _mm256_cmp_pd(abs_prod, toolow,  _CMP_LE_OS);
-  int high_mask_bits = _mm256_movemask_pd(high_mask);
-  int low_mask_bits  = _mm256_movemask_pd(low_mask);
   
-  if (high_mask_bits) {
+  if (!_mm256_testz_pd(high_mask, high_mask)) [[unlikely]] { 
+    int high_mask_bits = _mm256_movemask_pd(high_mask);
     exponent += _mm_popcnt_u32(high_mask_bits);
     abs_prod = _mm256_blendv_pd(abs_prod, _mm256_mul_pd(abs_prod, toolow), high_mask);  
   }
   
-  if (low_mask_bits) {
+  if (!_mm256_testz_pd(low_mask, low_mask)) [[unlikely]] {
+    int low_mask_bits  = _mm256_movemask_pd(low_mask);
     exponent -= _mm_popcnt_u32(low_mask_bits);
     abs_prod = _mm256_blendv_pd(abs_prod, _mm256_mul_pd(abs_prod, toohigh), low_mask);
   }
