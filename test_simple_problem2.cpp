666d3f492614bb902552ff21e082e61f75741989
Micha Riser <micha@classtime.com>
2023-06-10 21:53:24 +0200

    Fix tests and adjust code.

--- test_simple_problem2.cpp
+++ test_simple_problem2.cpp
@@ -23,18 +23,6 @@ g++ -std=c++11 -O3 -funroll-loops -march=native -lm -o test_simple_problem2 test
 using namespace std;
 
 
-class stopwatch;
-
-inline double sqr(const double x);
-void init_random_positions(const long int N, const double a, const double b, double * x);
-inline void checkoverflow(double &prod, long int &exponent);
-LargeExponentFloat prod_realreal(const long int N, const long int k, const double u, const double * x, LargeExponentFloat prod);
-void prod_realcomplex(const long int N, const double u, const double * x, const 
-double * y, double &prod, long int &exponent);
-void prod_complexreal(const long int N, const double u, const double v, const double * x, double &prod, long int &exponent);
-void prod_complexcomplex(const long int N, const long int k, const double u, const double v, const double * x, const double * y, double &prod, long int &exponent);
-
-
 static std::mt19937_64 gen;
 static std::uniform_real_distribution<double> distu(0.0, 1.0);
 
@@ -46,9 +34,6 @@ void init_random_positions(const long int N, const double a, const double b, dou
   for (int j=0; j<N; j++) x[j]=distu(gen)*(b-a)+a;
   return;
 }
-
-
-
 
 class stopwatch {
   private:
@@ -200,14 +185,13 @@ __m256d sqr_diff(__m256d x, __m256d y, __m256d u, __m256d v) {
 }
 
 
-void prod_complexcomplex(const long int N, const long int k, const double u1, const double u2, const double v1, const double v2, const double * x, const double * y, double &prod1_ref, long int &exponent1_ref, double &prod2_ref, long int &exponent2_ref) {
-
+void prod_complexcomplex(const long int N, const long int k, const double u1, const double u2, const double v1, const double v2, const double * x, const double * y, LargeExponentFloat& prod1, LargeExponentFloat& prod2) {
   const int64_t ELEMENTS_PER_LOOP = 4 * 4;
   assert(N % ELEMENTS_PER_LOOP == 0);
   assert(reinterpret_cast<uintptr_t>(x) % 32 == 0);
 
-  VProd prod1(prod1_ref, exponent1_ref);
-  VProd prod2(prod2_ref, exponent2_ref);
+  VProd vprod1(prod1.significand, prod1.exponent);
+  VProd vprod2(prod2.significand, prod2.exponent);
 
   __m256d u1_vec = _mm256_set1_pd(u1);
   __m256d u2_vec = _mm256_set1_pd(u2);
@@ -229,13 +213,13 @@ void prod_complexcomplex(const long int N, const long int k, const double u1, co
       const __m256d y2 = _mm256_load_pd(&y[j +  8]);
       const __m256d y3 = _mm256_load_pd(&y[j + 12]);
 
-      prod1.mul_no_overflow(
+      vprod1.mul_no_overflow(
         sqr_diff(x0, y0, u1_vec, v1_vec),
         sqr_diff(x1, y1, u1_vec, v1_vec),
         sqr_diff(x2, y2, u1_vec, v1_vec),
         sqr_diff(x3, y3, u1_vec, v1_vec)
       );
-      prod2.mul_no_overflow(
+      vprod2.mul_no_overflow(
         sqr_diff(x0, y0, u2_vec, v2_vec),
         sqr_diff(x1, y1, u2_vec, v2_vec),
         sqr_diff(x2, y2, u2_vec, v2_vec),
@@ -244,37 +228,25 @@ void prod_complexcomplex(const long int N, const long int k, const double u1, co
     }
 
     if ((j / ELEMENTS_PER_LOOP) % 8 == 0) {
-      prod1.check_overflow();
-      prod2.check_overflow();
+      vprod1.check_overflow();
+      vprod2.check_overflow();
     }
   }
 
-  prod1.check_overflow();
-  prod2.check_overflow();
+  vprod1.check_overflow();
+  vprod2.check_overflow();
 
-  auto prod = prod1.get();
-  prod1_ref = abs(prod.significand);
-  exponent1_ref = prod.exponent;
-
-  prod = prod2.get();
-  prod2_ref = abs(prod.significand);
-  exponent2_ref = prod.exponent;
+  prod1 = vprod1.get();
+  prod2 = vprod2.get();
 
   for (int j=skipj; j<skipj + ELEMENTS_PER_LOOP; j++) {
     if (j == k) {
       continue;
     }
-    prod1_ref *= sqr(u1-x[j])+sqr(v1-y[j]);
-    prod2_ref *= sqr(u2-x[j])+sqr(v2-y[j]);
-    checkoverflow(prod1_ref, exponent1_ref);
-    checkoverflow(prod2_ref, exponent2_ref);
+    prod1 = save_mul(prod1, sqr(u1-x[j])+sqr(v1-y[j]));
+    prod2 = save_mul(prod2, sqr(u2-x[j])+sqr(v2-y[j]));
   }
  }
-
-
-
-
-
 
 // **************************************************************************
 
@@ -302,7 +274,7 @@ void test_realreal() {
     assert_approx(2.11219e-81, prod2.significand);
     assert_eq(-45 * 511L, prod2.exponent);
    
-    delete(x);
+    delete[] x;
   }
 
 }
@@ -317,19 +289,20 @@ void test_complexcomplex() {
     init_random_positions(N,-1,1,x);
     init_random_positions(N,-1,1,y);
 
-    double prod1 = 7.1;
-    int64_t exponent1 = 42;
-    double prod2 = 0.02;
-    int64_t exponent2 = -2;
+    LargeExponentFloat prod1(7.1, 42 * 511);
+    LargeExponentFloat prod2(0.02, -2 * 511);
 
-    prod_complexcomplex(N, 2122, 1.4334, 0.1233, -2.13, 0.111, x, y, prod1, exponent1, prod2, exponent2);
+    prod_complexcomplex(N, 2122, 1.4334, 0.1233, -2.13, 0.111, x, y, prod1, prod2);
 
-    assert_approx(161.905, prod1);
-    assert_eq(127L, exponent1);
-    assert_approx(6.29374e-112, prod2);
-    assert_eq(-34L, exponent2);
+    cout << prod1 << endl;
 
-    delete(x);
+    assert_approx(161.905, prod1.significand);
+    assert_eq(127 * 511L, prod1.exponent);
+    assert_approx(6.29374e-112, prod2.significand);
+    assert_eq(-34 * 511L, prod2.exponent);
+
+    delete[] x;
+    delete[] y;
   }
 
 }
@@ -386,7 +359,7 @@ int main(int argc, char *argv[]) {
     double v=distu(gen)*2-1;
     double u0=distu(gen)*2-1;
     double v0=distu(gen)*2-1;
-    prod_complexcomplex(N,k,u,u0,v,v0,x,y,prod.significand,prod.exponent,prod0.significand,prod0.exponent);
+    prod_complexcomplex(N,k,u,u0,v,v0,x,y,prod,prod0);
   }
   timing.stop();
   cout << "prod_complexcomplex: prod=" << prod.significand/prod0.significand << " exponent=" << prod.exponent-prod0.exponent << " timing=" << timing.get_time() << " seconds\n";
